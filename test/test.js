f3766faa0d4dd135e10824a99bfc02bdaf4ba065
derrick <haylederrick14@gmail.com>
2025-07-05 18:33:09 +0300

    modify test file

---
+++ test/test.js
@@ -0,0 +1,19 @@
+import { assert } from "chai";
+import multiply from "../multiply.js";
+describe("making test for multiplicity", function () {
+  it("multiplying two numbers", function () {
+    assert.equal(multiply(1, 1), 1);
+  });
+  it("another test with two numbers", function () {
+    assert.equal(multiply(2, 2), 4);
+  });
+  it("multiplying 3 * 3", function () {
+    assert.equal(multiply(3, 3), 9);
+  });
+  it("multiplying 4 * 4", function () {
+    assert.equal(multiply(4, 4), 16);
+  });
+  it("multiplying 23, 45", function () {
+    assert.equal(multiply(23, 45), 23 * 45);
+  });
+});
