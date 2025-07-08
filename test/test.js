80c2fc058db2a8aa55cd836a0129ee2e3d0c260c
derrick <haylederrick14@gmail.com>
2025-07-08 20:03:59 +0300

    update test file

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 import { assert } from "chai";
 import factorial from "../factorial.js";
+import fibonacci from "../fibonacci.js";
 import multiply from "../multiply.js";
 // team member
 // 1. Okongo Derrick
@@ -38,5 +39,29 @@ describe("making test for factorial", function () {
   });
   it("factorial of 4", function () {
     assert.equal(factorial(4), 24);
+  });
+});
+
+describe("making test for fibonacci", () => {
+  it("fibonacci of 0", () => {
+    assert.equal(fibonacci(0), 0);
+  });
+  it("fibonacci of 1", function () {
+    assert.equal(fibonacci(1), 1);
+  });
+  it("fibonacci of 2", function () {
+    assert.equal(fibonacci(2), 1);
+  });
+  it("fibonacci of 3", function () {
+    assert.equal(fibonacci(3), 2);
+  });
+  it("fibonacci of 4", function () {
+    assert.equal(fibonacci(4), 3);
+  });
+  it("fibonacci of 5", function () {
+    assert.equal(fibonacci(5), 5);
+  });
+  it("fibonacci of 6", function () {
+    assert.equal(fibonacci(6), 8);
   });
 });
